f8414cdce202ee5cf09da30cae2d5ac416c66566
Javier Bass <catxd03@gmail.com>
2025-08-14 21:38:11 -0400

    agregado una funcion de cachipun, mejorable

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,5 @@
+import random
+
 def sumar (numero1, numero2):
     resultado= numero1+numero2
     return resultado
@@ -10,6 +12,30 @@ def multiplicacion (numero1, numero2):
     resultado = numero1*numero2
     return resultado
 
-def dividir (numero1, numero2)
+def dividir (numero1, numero2):
     resultado = numero1//numero2
     return resultado
+
+cachipun = random.randint(1,3)
+
+def cachipun1v1 (eleccion):
+    maquina = random.randint(1,3)
+    #1 es tijera, 2 es roca, 3 es papel
+    if maquina == 1 and eleccion == 3:
+        return perder
+    if maquina == 2 and eleccion == 3:
+        return ganar
+    if maquina == 3 and eleccion == 3:
+        return empate
+    if maquina == 1 and eleccion == 2:
+        return perder
+    if maquina == 2 and eleccion == 2:
+        return ganar
+    if maquina == 3 and eleccion == 2:
+        return empate
+    if maquina == 1 and eleccion == 2:
+        return perder
+    if maquina == 2 and eleccion == 2:
+        return ganar
+    if maquina == 3 and eleccion == 2:
+        return empate
